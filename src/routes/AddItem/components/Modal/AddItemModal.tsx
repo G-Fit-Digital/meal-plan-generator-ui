3cbecfd8e1fe045a0352defb22a6ac7cc9822fc8
Joe Sworyk <joesworyk@Joes-MacBook-Pro.local>
2019-09-02 22:06:51 -0400

    <button to add item>

--- src/routes/AddItem/components/Modal/AddItemModal.tsx
+++ src/routes/AddItem/components/Modal/AddItemModal.tsx
@@ -138,6 +138,22 @@ export default ({ isOpen }: any) => {
             placeholder="Meal Category"
           />
         </div>
+        <button
+          style={{
+            marginTop: 25,
+            backgroundColor: "blue",
+            height: 48,
+            width: 150,
+            cursor: "pointer",
+            fontSize: 20,
+            color: "#FFF",
+            fontWeight: "bold",
+            border: "none",
+            borderRadius: 25
+          }}
+        >
+          Add Item
+        </button>
       </div>
     </Modal>
   );
