5b4b9a1290f9a69712005fa93caff5213c797159
Joe Sworyk <joesworyk@Joes-MacBook-Pro.local>
2019-09-04 23:04:18 -0400

    <Front end input fields>

--- src/routes/DietGenerator/DietGenerator.tsx
+++ src/routes/DietGenerator/DietGenerator.tsx
@@ -1,9 +1,90 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { calorieMaintenanceCalculation } from "../../utils/calorieCalculator";
 
 export default () => {
+  useEffect(() => {
+    let val = calorieMaintenanceCalculation(135, 25, "female", 21, 1.375, 0.5);
+    console.log(val);
+  });
   return (
-    <div>
-      <h1>MEAL PLAN GENERATOR</h1>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <input
+        style={{
+          height: 48,
+          marginBottom: 8,
+          width: 375,
+          alignSelf: "center",
+          marginTop: 8,
+        }}
+        type="text"
+        placeholder="Client Name"
+      />
+      <input
+        style={{
+          height: 48,
+          marginBottom: 8,
+          width: 375,
+          alignSelf: "center",
+          marginTop: 8,
+        }}
+        type="text"
+        placeholder="Client Weight (Pounds)"
+      />
+      <input
+        style={{
+          height: 48,
+          marginBottom: 8,
+          width: 375,
+          alignSelf: "center",
+          marginTop: 8,
+        }}
+        type="text"
+        placeholder="Client BF (%)"
+      />
+      <input
+        style={{
+          height: 48,
+          marginBottom: 8,
+          width: 375,
+          alignSelf: "center",
+          marginTop: 8,
+        }}
+        type="text"
+        placeholder="Client Gender"
+      />
+      <input
+        style={{
+          height: 48,
+          marginBottom: 8,
+          width: 375,
+          alignSelf: "center",
+          marginTop: 8,
+        }}
+        type="text"
+        placeholder="Client Age"
+      />
+      <input
+        style={{
+          height: 48,
+          marginBottom: 8,
+          width: 375,
+          alignSelf: "center",
+          marginTop: 8,
+        }}
+        type="text"
+        placeholder="Activity Level"
+      />
+      <input
+        style={{
+          height: 48,
+          marginBottom: 8,
+          width: 375,
+          alignSelf: "center",
+          marginTop: 8,
+        }}
+        type="text"
+        placeholder="Aggressiveness"
+      />
     </div>
   );
 };
